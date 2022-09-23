95416c1cd7ffe24b5498fa43c1fbc028815e1714
Jahana Sherin K <110897451+SherinJahana@users.noreply.github.com>
2022-09-23 09:13:05 +0330

    Add files via upload

---
+++ word.py
@@ -0,0 +1,8 @@
+for i in range (1,5):
+    for j in range(1,i+1):
+        print(j,end="") 
+    print("\n")
+for i in range (5,0,-1):
+    for j in range(1,i+1):
+        print(j,end="") 
+    print("\n")
