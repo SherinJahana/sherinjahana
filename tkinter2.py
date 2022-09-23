407c6cc4898614d460100eb9604226ab86d221da
Jahana Sherin K <110897451+SherinJahana@users.noreply.github.com>
2022-09-23 09:32:17 +0330

    Add files via upload

---
+++ tkinter2.py
@@ -0,0 +1,13 @@
+import turtle
+t=turtle.Turtle()
+
+t.speed(10)
+for i in range(36):
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(100)
