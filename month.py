02f8a18123b7f8281d92ba0bf4ab50723af1c008
Jahana Sherin K <110897451+SherinJahana@users.noreply.github.com>
2022-09-23 09:24:58 +0330

    Add files via upload

---
+++ month.py
@@ -0,0 +1,10 @@
+import math
+
+x1=int(input("enter x1 = "))
+y1=int(input("enter y1 = "))
+x2=int(input("enter x2 = "))
+y2=int(input("enter y2 = "))
+
+print("distamce btw tvm and kochi = ",math.sqrt(math.pow((x2-x1),2)+math.pow((y2-y1),2)))
+
+print("slope =",(y2-y1)/(x2-x1))
