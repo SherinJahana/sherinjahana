ac57ef3df797903ca33341ed57158a9581f40f30
Jahana Sherin K <110897451+SherinJahana@users.noreply.github.com>
2022-09-23 09:30:14 +0330

    Add files via upload

---
+++ script.py
@@ -0,0 +1,14 @@
+import sys,datetime,os,socket
+
+print("="*70)
+print('this system version is',sys.version)
+print("="*70)
+print("this system os is ",sys.platform)
+print("="*70)
+print("the current date and time is ",datetime.datetime.now())
+print("="*70)
+print("current user",os.getlogin())
+print("="*70)
+print(os.name)
+print("="*70)
+print("="*70)
